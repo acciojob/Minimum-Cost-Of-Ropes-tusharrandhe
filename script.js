6f8e1e93120bee5055e8dd6dc5f3fa28df53f2b8
Tushar Randhe <121237922+tusharrandhe@users.noreply.github.com>
2023-02-15 22:56:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your code here
+function minCostToFormRope(event)
+{
+	event.preventDefault();
+	let input = document.querySelector("input").value;
+	let arr = input.split(",");
+	arr.sort(function(a,b){return a-b});
+
+	let cost = 0;
+	while (arr.length > 1) {
+		let result = Number(arr[0]) + Number(arr[1]);
+		arr.splice(0,2);
+		arr.push(result);
+		cost = cost + result;
+		arr.sort(function(a,b){return a-b});
+	}
+	document.getElementById("result").textContent = cost;
+}
